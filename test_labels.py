f934b7f0edba9f986ea00ec796c1ed248cd806d7
Jean-Michel Bodart <62448990+jmbo1190@users.noreply.github.com>
2021-10-31 09:14:22 +0000

    test_labels.py: add test_cvcli()

--- test_labels.py
+++ test_labels.py
@@ -1,17 +1,31 @@
 from labels import labels
+from click.testing import CliRunner
+from cvcli import calllabels
+
 
 def test_lion():
     myLabels = labels("cloud-comp-found-function-bike-rider", "lion.jpg")
-    assert type( myLabels) == list
+    assert type(myLabels) == list
     label1 = myLabels[0]
     assert type(label1) == dict
     assert label1.get("Name") == "Lion"
     assert label1.get("Confidence") >= 0.98
 
+
 def test_tiger():
     myLabels = labels("cloud-comp-found-function-bike-rider", "tiger.jpg")
-    assert type( myLabels) == list
+    assert type(myLabels) == list
     label1 = myLabels[0]
     assert type(label1) == dict
     assert label1.get("Name") == "Tiger"
     assert label1.get("Confidence") >= 0.98
+
+
+def test_cvcli():
+    runner = CliRunner()
+    result = runner.invoke(
+        calllabels,
+        ["--bucket", "cloud-comp-found-function-bike-rider", "--name", "lion.jpg"],
+    )
+    assert result.exit_code == 0
+    assert "Lion" in result.output
