037b6d61a315aaee29117dd5a496b656245b48ca
Jean-Michel Bodart <62448990+jmbo1190@users.noreply.github.com>
2021-10-31 09:13:31 +0000

    cvcli: call labels()

--- cvcli.py
+++ cvcli.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python
 import click
-import boto3
+
+# import boto3
+from labels import labels
 
 
 @click.command()
-@click.option("--bucket", 
-	prompt="Pass in the S3 Bucket (e.g. cloud-comp-found-function-bike-rider)", 
-	help="This is the S3 Bucket")
+@click.option(
+    "--bucket",
+    prompt="Pass in the S3 Bucket (e.g. cloud-comp-found-function-bike-rider)",
+    help="This is the S3 Bucket",
+)
 @click.option(
     "--name",
     help="This is the name of the image",
     prompt="Pass in the name (e.g. lion.jpg)",
 )
-def labels(bucket, name):
-    """This takes an S3 bucket and a image name"""
+def calllabels(bucket, name):
+    """CLI call to Computer Vision function labels() \n
+    example usage: \n
+       cvcli.py --help
+       cvcli.py --bucket "cloud-comp-found-function-bike-rider" --name "Lion.jpg"\n
+    """
 
-    print(f"This is the bucketname {bucket} !")
-    print(f"This is the imagename {name} !")
-    rekognition = boto3.client("rekognition")
-    response = rekognition.detect_labels(
-        Image={
-            "S3Object": {
-                "Bucket": bucket,
-                "Name": name,
-            }
-        },
-    )
-    labels = response["Labels"]
+    # print(f"This is the bucketname {bucket} !")
+    # print(f"This is the imagename {name} !")
+    found_labels = labels(bucket, name)
     click.echo(click.style("Found Labels:", fg="red"))
-    for label in labels:
+    for label in found_labels:
         click.echo(click.style(f"{label}", bg="blue", fg="white"))
 
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
-    labels()
+    calllabels()
