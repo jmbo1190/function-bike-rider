954bee4982932b30580afbfb83cb192581ae342d
EC2 Default User <ec2-user@ip-172-31-18-2.eu-central-1.compute.internal>
2021-10-31 06:59:26 +0000

    reformatted python files with black

--- hellocli.py
+++ hellocli.py
@@ -3,25 +3,26 @@ import click
 from hello import marco
 import sys
 
+
 @click.command()
-@click.option('--name')
+@click.option("--name")
 def callmarco(name):
-    """ CLI call to function marco() \n
-        usage: \n
-           hellocli.py --name "Marco"\n
-           hellocli.py --help
+    """CLI call to function marco() \n
+    usage: \n
+       hellocli.py --name "Marco"\n
+       hellocli.py --help
     """
     result = marco(name)
     print(f"Debug(callmarco): name: {name}, result: {result}")
     if result != "Polo":
-        click.echo(click.style(f'{result}', bg='red'))
+        click.echo(click.style(f"{result}", bg="red"))
         sys.exit(1)  # Exit with status 1 - can be displayed in Linux with 'echo $?'
         # return True
-    click.echo(click.style(f'{result}', bg='green')) 
+    click.echo(click.style(f"{result}", bg="green"))
     # return True
     # normal exit with status 0
 
-if __name__ == '__main__':
-    #pylint: disable=no-value-for-parameter
-    callmarco()
 
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    callmarco()
--- test_hello.py
+++ test_hello.py
@@ -2,20 +2,24 @@ from click.testing import CliRunner
 from hello import marco
 from hellocli import callmarco
 
+
 def test_marco():
     assert "Polo" == marco("Marco")
+
 
 def test_not_marco():
     assert "No!" == marco("Bobo")
 
+
 def test_not_marco_cli():
-  runner = CliRunner()
-  result = runner.invoke(callmarco, ['--name', 'Bob'])
-  assert result.exit_code == 1
-  assert 'No!' in result.output
+    runner = CliRunner()
+    result = runner.invoke(callmarco, ["--name", "Bob"])
+    assert result.exit_code == 1
+    assert "No!" in result.output
+
 
 def test_marco_cli():
-  runner = CliRunner()
-  result = runner.invoke(callmarco, ['--name', 'Marco'])
-  assert result.exit_code == 0
-  assert 'Polo' in result.output
+    runner = CliRunner()
+    result = runner.invoke(callmarco, ["--name", "Marco"])
+    assert result.exit_code == 0
+    assert "Polo" in result.output
